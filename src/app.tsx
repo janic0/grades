cdfc3a01020d74185b1e127f7c380cbe13d83fd5
janic0 <prodjnc@gmail.com>
2022-02-07 09:59:25 +0100

    added better logic for no grades

--- src/app.tsx
+++ src/app.tsx
@@ -21,7 +21,6 @@ const App = () => {
 	const [data, setData] = useState<data[] | undefined>(undefined);
 	const [focusData, setFocusData] = useState<data | undefined>(undefined);
 	useEffect(() => {
-		setData([]);
 		fetch("https://grades.janic.io/", { method: "GET" }).then((response) => {
 			response.json().then((data) => {
 				setData(data.data);
@@ -49,68 +48,88 @@ const App = () => {
 						<div className="w-full md:w-1/2 bg-slate-500 rounded p-6">
 							<h1 className="text-3xl">{focusData.description}</h1>
 							<h1>{focusData.title}</h1>
-							<div
-								className={
-									"text-center mt-5 p-4 rounded " +
-									(parseFloat(focusData.average) < 5
-										? "bg-orange-300"
-										: parseFloat(focusData.average) < 4
-										? "bg-red-500"
-										: "bg-green-500")
-								}
-							>
-								{focusData.average}
-							</div>
-							<table className="mt-5 w-full">
-								<thead>
-									<tr>
-										<td className="hidden md:block">Datum</td>
-										<td>Topic</td>
-										<td>Grade</td>
-										<td className="hidden md:block">Durchschnitt</td>
-										<td>Weight</td>
-									</tr>
-								</thead>
-								<tbody>
-									{focusData.items.map((item, i) => (
-										<tr key={i} className="">
-											<td className="hidden md:block">{item.datum}</td>
-											<td>{item.thema}</td>
-											<td>{item.bewertung}</td>
-											<td className="hidden md:block">{item.schnitt}</td>
-											<td>{item.gewichtung}</td>
+							{() => {
+								const average = parseFloat(focusData.average);
+								<div
+									className={
+										"text-center mt-5 p-4 rounded " +
+										(isNaN(average)
+											? "bg-gray-500"
+											: average < 5
+											? "bg-orange-300"
+											: average < 4
+											? "bg-red-500"
+											: "bg-green-500")
+									}
+								>
+									{focusData.average}
+								</div>;
+							}}
+							{focusData.items.length ? (
+								<table className="mt-5 w-full">
+									<thead>
+										<tr>
+											<td className="hidden md:block">Datum</td>
+											<td>Topic</td>
+											<td>Grade</td>
+											<td className="hidden md:block">Durchschnitt</td>
+											<td>Weight</td>
 										</tr>
-									))}
-								</tbody>
-							</table>
+									</thead>
+									<tbody>
+										{focusData.items.map((item, i) => (
+											<tr key={i} className="">
+												<td className="hidden md:block">{item.datum}</td>
+												<td>{item.thema}</td>
+												<td>{item.bewertung}</td>
+												<td className="hidden md:block">{item.schnitt}</td>
+												<td>{item.gewichtung}</td>
+											</tr>
+										))}
+									</tbody>
+								</table>
+							) : (
+								<div className="text-center bg-gray-600 p-4 mt-4 rounded">
+									no grades yet
+								</div>
+							)}
 						</div>
 					</div>
 				) : null}
 				<div className="flex gap-4 flex-wrap">
-					{data.map((d, i) => (
-						<div
-							onClick={() => setFocusData(d)}
-							className={
-								"p-6 rounded hover:shadow-2xl translate hover:shadow-slate-300 dark:hover:shadow-black duration-150 cursor-pointer motion-safe:hover:-translate-y-1 transition-all w-full md:w-fit " +
-								(parseFloat(d.average) < 5
-									? "bg-orange-300"
-									: parseFloat(d.average) < 4
-									? "bg-red-500"
-									: "bg-green-500")
-							}
-							key={i}
-						>
-							<h1>{d.title}</h1>
-							<p className="text-slate-800 dark:text-slate-200">
-								{d.description}
-							</p>
-						</div>
-					))}
+					{data.map((d, i) => {
+						const average = parseFloat(d.average);
+						return (
+							<div
+								onClick={() => setFocusData(d)}
+								className={
+									"p-6 rounded hover:shadow-2xl translate hover:shadow-slate-300 dark:hover:shadow-black duration-150 cursor-pointer motion-safe:hover:-translate-y-1 transition-all w-full md:w-fit " +
+									(isNaN(average)
+										? "bg-gray-500"
+										: average < 5
+										? "bg-orange-300"
+										: average < 4
+										? "bg-red-500"
+										: "bg-green-500")
+								}
+								key={i}
+							>
+								<h1>{d.title}</h1>
+								<p className="text-slate-800 dark:text-slate-200">
+									{d.description}
+								</p>
+							</div>
+						);
+					})}
 				</div>
 			</div>
 		);
 	} else {
-		return <div className="dark:text-white">Loading...</div>;
+		return (
+			<div className="h-screen w-screen flex justify-center items-center">
+				<p className="dark:text-white animate-spin text-8xl text-center">◠</p>;
+			</div>
+		);
 	}
 };
 
